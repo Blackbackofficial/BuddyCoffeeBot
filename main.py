bbfd0410ae9b5147504518139d1db006185da59d
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2022-04-24 00:06:17 +0300

    Add network career

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import telebot
 from telebot import types
-from const import WELCOME_MESS, ABOUT
+from const import WELCOME_MESS, ABOUT, NETWORK_CAREER
 
-bot = telebot.TeleBot()
+bot = telebot.TeleBot("")
 
 
 def main():
     @bot.message_handler(commands=['start'])
     def send_welcome(message):
-        markup = types.InlineKeyboardMarkup(row_width=1)
+        markup = types.InlineKeyboardMarkup()
         btn1 = types.InlineKeyboardButton('Заполнить анкету', callback_data='questionnaire')
         btn2 = types.InlineKeyboardButton('Узнать подробнее о чат-боте', callback_data='about')
         btn3 = types.InlineKeyboardButton('Карьерные социальные сети ГК "Росатом"', callback_data="network_career")
-        btn4 = types.InlineKeyboardButton('Ближайшие карьерные мероприятия ГК "Росатом"', callback_data="career_events")
-        markup.add(btn1, btn2, btn3, btn4)
+        markup.row(btn1, btn2)
+        markup.row(btn3)
         bot.send_message(message.chat.id, text=WELCOME_MESS, reply_markup=markup)
 
     @bot.callback_query_handler(func=lambda call: True)
@@ -23,8 +23,20 @@ def main():
                 bot.send_message(call.message.chat.id, text="Привеет.. Спасибо что читаешь статью!)")
             elif call.data == 'about':
                 bot.send_message(call.message.chat.id, text=ABOUT)
+            elif call.data == 'network_career':
+                markup = types.InlineKeyboardMarkup()
+                btn1 = types.InlineKeyboardButton('ВКонтакте', url='https://vk.com/rosatomcareer')
+                btn2 = types.InlineKeyboardButton('Instagram', url='https://www.instagram.com/rosatom_career/?hl=ru')
+                btn3 = types.InlineKeyboardButton('Назад', callback_data="back")
+                markup.row(btn1, btn2)
+                markup.row(btn3)
+                bot.send_message(call.message.chat.id, text=NETWORK_CAREER, reply_markup=markup)
+            elif call.data == 'back':
+                send_welcome(call.message)
+            else:
+                bot.send_message(call.message.chat.id, text="Что-то пошло не так.")
 
-    bot.polling()
+    bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
